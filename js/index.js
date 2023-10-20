9fda585c96855c4e50a93ec920b031ac3be6bb51
Hellaim <hellaimsunlane@gmail.com>
2023-10-20 18:41:23 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // code here
+    let $resolutionResult = document.querySelector('.js_resalution-result-self')
+    let $resolutionResultBox = document.querySelector('.js_resalution-result-box')
+    let viewportWidth = window.innerWidth
+    let resolutionBoxWidth = $resolutionResultBox.offsetWidth
+    $resolutionResult.innerHTML = `${viewportWidth} px`
+    $resolutionResultBox.innerHTML = `${viewportWidth} px`
+
+    window.addEventListener('resize', () => {
+        let $resolutionResult = document.querySelector('.js_resalution-result-self')
+        let $resolutionResultBox = document.querySelector('.js_resalution-result-box')
+        let viewportWidth = window.innerWidth
+        let resolutionBoxWidth = $resolutionResultBox.offsetWidth
+        $resolutionResult.innerHTML = `${viewportWidth} px`
+        $resolutionResultBox.innerHTML = `${viewportWidth} px`
+    })
 })
+
